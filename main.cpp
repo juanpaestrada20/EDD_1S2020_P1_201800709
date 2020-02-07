c1305ace23847088c555780c6645be0bd02a5aeb
Juan Pablo Estrada <3599546500101@ingenieria.usac.edu.gt>
2020-02-07 09:33:14 -0600

    Creacion Menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
+
+void menu();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    menu();
     return 0;
+}
+
+void menu() {
+    int opcion;
+    do{
+        cout << "\tUNIVERSIDAD SAN CARLOS DE GUATEMALA" << endl;
+        cout << "\tFACULTAD DE INGENIERIA" << endl;
+        cout << "\tESTRUCTURAS DE DATOS" << endl;
+        cout << "\tPRACTICA 1" << endl;
+        cout << "\tJUAN PABLO ESTRADA ALEMAN" << endl;
+        cout << "\t201800709" << endl;
+        cout << "\n" << endl;
+        cout << "\t\tMENU" << endl;
+        cout << "1. Crear Archivo" << endl;
+        cout << "2. Abrir Archivo" << endl;
+        cout << "3. Archivos Recientes" << endl;
+        cout << "4. Salir" << endl;
+
+        cin >> opcion;
+
+    }while (opcion != 4);
 }
