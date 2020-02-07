1494669c04345700640e798cfc0f1e1256587d71
Juan Pablo Estrada Aleman <3599546500101@ingenieria.usac.edu.gt>
2020-02-07 09:12:33 -0600

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Practica1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practica1.dir/main.cpp.obj"
+  "Practica1.exe"
+  "Practica1.exe.manifest"
+  "Practica1.pdb"
+  "libPractica1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practica1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
